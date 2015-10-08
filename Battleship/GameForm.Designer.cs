b0425624152cfb201adb55e3ab74899f730f217a
Aareschluchtje <remcosannen@gmail.com>
2015-10-08 12:41:04 +0200

    Mijn zooi

--- Battleship/GameForm.Designer.cs
+++ Battleship/GameForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Battleship
+﻿using System.Drawing;
+
+namespace Battleship
 {
     partial class GameForm
     {
@@ -28,20 +30,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // pictureBox1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(646, 514);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(770, 585);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(670, 538);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "GameForm";
+            this.Text = "Battleship";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Battleship/GameForm.cs
+++ Battleship/GameForm.cs
@@ -15,6 +15,22 @@ namespace Battleship
         public GameForm()
         {
             InitializeComponent();
+            draw();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void draw()
+        {
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            for(int i = 0; i < 10; i++)
+            {
+                g.DrawLine(new Pen(Color.Black, 10), new Point(i*100, 0), new Point(i*100, 800));
+            }
+        }
+        
     }
 }
